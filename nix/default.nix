069c555e9fdb7dc59da0c1900bc082f031278d2e
Potato Hatsue <1793913507@qq.com>
2021-12-20 15:35:40 -0500

    Update nix drv

--- nix/default.nix
+++ nix/default.nix
@@ -1,8 +1,9 @@
 { mkDerivation, aeson, async, base, binary, binary-instances
 , bytestring, containers, data-default, extra, free, hspec
 , hspec-discover, lib, microlens, microlens-th, neat-interpolation
-, optparse-simple, parsec, shake, stm, text, tomland, transformers
-, unliftio, unordered-containers, validation-selective
+, optparse-simple, parsec, regex-tdfa, shake, stm, text, tomland
+, transformers, unliftio, unordered-containers
+, validation-selective
 }:
 mkDerivation {
   pname = "nvfetcher";
@@ -13,20 +14,20 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson base binary binary-instances bytestring containers
     data-default extra free microlens microlens-th neat-interpolation
-    optparse-simple parsec shake text tomland transformers
+    optparse-simple parsec regex-tdfa shake text tomland transformers
     unordered-containers
   ];
   executableHaskellDepends = [
     aeson base binary binary-instances bytestring containers
     data-default extra free microlens microlens-th neat-interpolation
-    optparse-simple parsec shake text tomland transformers
+    optparse-simple parsec regex-tdfa shake text tomland transformers
     unordered-containers validation-selective
   ];
   testHaskellDepends = [
     aeson async base binary binary-instances bytestring containers
     data-default extra free hspec microlens microlens-th
-    neat-interpolation optparse-simple parsec shake stm text tomland
-    transformers unliftio unordered-containers
+    neat-interpolation optparse-simple parsec regex-tdfa shake stm text
+    tomland transformers unliftio unordered-containers
   ];
   testToolDepends = [ hspec-discover ];
   homepage = "https://github.com/berberman/nvfetcher";
