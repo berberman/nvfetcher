d00561ce893fdb75067ba4488f97b28d07e7b91b
Potato Hatsue <1793913507@qq.com>
2022-01-29 16:39:16 -0500

    Produce parser readable generated.json

--- src/NvFetcher/Utils.hs
+++ src/NvFetcher/Utils.hs
@@ -6,6 +6,7 @@ import qualified Data.ByteString.Lazy as LBS
 import Data.Text (Text)
 import qualified Data.Text as T
 import Development.Shake
+import Text.Regex.TDFA ((=~))
 
 encode' :: Binary a => a -> BS.ByteString
 encode' = BS.concat . LBS.toChunks . encode
@@ -18,3 +19,11 @@ quote = T.pack . show
 
 getShakeDir :: Action FilePath
 getShakeDir = shakeFiles <$> getShakeOptions
+
+isLegalNixId :: Text -> Bool
+isLegalNixId x = x =~ "^[a-zA-Z_][a-zA-Z0-9_'-]*$"
+
+quoteIfNeeds :: Text -> Text
+quoteIfNeeds x
+  | isLegalNixId x = x
+  | otherwise = quote x
