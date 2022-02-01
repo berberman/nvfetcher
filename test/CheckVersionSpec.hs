96a5344bdf27da24e5c29a90db82085adfc34620
Potato Hatsue <1793913507@qq.com>
2022-01-31 21:51:51 -0500

    Add webpage test

--- test/CheckVersionSpec.hs
+++ test/CheckVersionSpec.hs
@@ -50,9 +50,13 @@ versionSourcesSpec = aroundShake $
       runNvcheckerRule (GitHubTag "harry-sanabria" "ReleaseTestRepo" $ def & ignored ?~ "second_release release3")
         `shouldReturnJust` Version "first_release"
 
-    specifyChan "http header" $ do
+    specifyChan "http header" $
       runNvcheckerRule (HttpHeader "https://www.unifiedremote.com/download/linux-x64-deb" "urserver-([\\d.]+).deb" def)
         >>= shouldBeJust
+
+    specifyChan "webpage" $
+      runNvcheckerRule (Webpage "http://ftp.vim.org/pub/vim/patches/7.3/" "7\\.3\\.\\d+" def)
+        `shouldReturnJust` Version "7.3.1314"
 
     specifyChan "manual" $
       runNvcheckerRule (Manual "Meow") `shouldReturnJust` Version "Meow"
